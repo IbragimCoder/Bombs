a9f389af1ea25398e02aff895385342b207b3402
IbragimCoder <ibragimabbasov05@gmail.com>
2023-12-28 03:23:39 +0300

    Update main.cpp
    fixed bug

--- main.cpp
+++ main.cpp
@@ -10,21 +10,21 @@
 
 TEST(bombs, bombTest1)
 {
-    std::vector<std::vector<int>>& bombs = { {2,1,3},{6,1,4} };
+    std::vector<std::vector<int>> bombs = { {2,1,3},{6,1,4} };
     int res = maxDetonatedBombs(bombs);
     ASSERT_EQ(2, res);
 }
 
 TEST(bombs, bombTest2)
 {
-    std::vector<std::vector<int>>& bombs = { {1,1,5},{10,10,5} };
+    std::vector<std::vector<int>> bombs = { {1,1,5},{10,10,5} };
     int res = maxDetonatedBombs(bombs);
     ASSERT_EQ(1, res);
 }
 
 TEST(bombs, bombTest3)
 {
-    std::vector<std::vector<int>>& bombs = { {1,2,3},{2,3,1}, {3,4,2},{4,5,3},{5,6,4} };
+    std::vector<std::vector<int>> bombs = { {1,2,3},{2,3,1}, {3,4,2},{4,5,3},{5,6,4} };
     int res = maxDetonatedBombs(bombs);
     ASSERT_EQ(5, res);
 }
